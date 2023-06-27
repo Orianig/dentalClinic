db051c91aac91a3a602794594de2a62879d14472
orianig <oriana.ig016@gmail.com>
2023-06-27 22:21:42 +0200

    refactor: change general problems - remove unnecessary items

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,74 +1,50 @@
 import axios from 'axios';
-import jwt_decode from 'jwt-decode';
+// import jwt_decode from 'jwt-decode';
 
 // defino constante url que contiene la dirreccion url base para las solicitudes
 const url = 'http://localhost:3000/'
 
 //se define una llamada que recibe el token como argumento
-const config = (token) => {
-    const headersConfig = {
-        headers: { 
-          "authorization": `Bearer ${token}`,  
-        }
-    };
-    return headersConfig
-}
+// const config = (token) => {
+//     const headersConfig = {
+//         headers: { 
+//           "authorization": `Bearer ${token}`,  
+//         }
+//     };
+//     return headersConfig
+// }
 
 //REGISTRO
-export const registerUsers = async (credentials) => {
+export const register = async (credentials) => {
     await axios.post(`${url}auth/register`,credentials)
-    const login = {
-        "name": credentials.email,
-        "lastName": credentials.email,
+    const newRegister = {
+        "name": credentials.name,
+        "lastName": credentials.lastName,
         "email": credentials.email,
         "password": credentials.password,
-        "phoneNumber": credentials.email,
+        "phoneNumber": credentials.phoneNumber,
     }
-    return await loginUsers(login)
+    return await login(newRegister)
 }
 
 //LOGIN
 //funcion que toma las credenciales del usuario
 export const login = async (credentials) => {
-    //el resultado de la solicitud se almacena en la constante "res"
-    const res = await axios.post(`${url}auth/login`, credentials)
-    //se crea un objeto "data" que contiene las credenciales del usuario
-    const data = {
-        credentials: {
-            "token": res.data.token,
-            "user": jwt_decode(res.data.token)
-        }
+    try {
+      const res = await axios.post(`${url}auth/login`, credentials);
+      console.log('Respuesta de inicio de sesión:', res.data);
+      return res.data.token;
+    } catch (error) {
+      console.error('Error en la solicitud de inicio de sesión:', error);
+      throw error;
     }
-    //se devuelve el objeto "data"
-    return data
-}
+  };
 
-// USER PROFILE
-export const getUserProfile = async(token) => {
-    const res = await axios.get(`${url}user/profile`, config(token))
-    return res.data
-}
 
-// ALL USERS PROFILE
-export const getAllUsersProfile = async(credentials, token) => {
-    const res = await axios.get(`${url}user/allProfiles`, credentials , config(token))
-    return res.data
-}
 
-// UPDATE PROFILE
-export const updateProfile = async(id,credentials, token) => {
-    const res = await axios.put(`${url}user/updateProfile`, credentials , config(token))
-    return res.data
-}
 
-// UPDATE PROFILES ADMIN
-export const updateProfileByAdmin = async(id,credentials, token) => {
-    const res = await axios.put(`${url}user/:${id}`, credentials , config(token))
-    return res.data
-}
 
-// DELETE PROFILE
-export const deleteUser = async(token, id) => {
-    const res = await axios.delete(`${url}/user/:${id}`, config(token))
-    return res.data
-}
+
+
+
+
---
+++ src/services/user.service.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+
+// defino constante url que contiene la dirreccion url base para las solicitudes
+const url = 'http://localhost:3000/'
+
+//se define una llamada que recibe el token como argumento
+const config = (token) => {
+    const headersConfig = {
+        headers: { 
+          "authorization": `Bearer ${token}`,  
+        }
+    };
+    return headersConfig
+}
+// USER PROFILE
+export const getUserProfile = async(token) => {
+    const res = await axios.get(`${url}user/profile`, config(token))
+    return res.data
+}
+
+// ALL USERS PROFILE
+export const getAllUsersProfile = async(credentials, token) => {
+    const res = await axios.get(`${url}user/allProfiles`, credentials , config(token))
+    return res.data
+}
+
+// UPDATE PROFILE
+export const updateProfile = async(credentials, token) => {
+    const res = await axios.put(`${url}user/updateProfile`, credentials , config(token))
+    return res.data
+}
+
+// UPDATE PROFILES ADMIN
+export const updateProfileByAdmin = async(id,credentials, token) => {
+    const res = await axios.put(`${url}user/:${id}`, credentials , config(token))
+    return res.data
+}
+
+// DELETE PROFILE
+export const deleteUser = async(token, id) => {
+    const res = await axios.delete(`${url}/user/:${id}`, config(token))
+    return res.data
+}
