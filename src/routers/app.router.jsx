34ad487004cf88588a3ab72e120536492aa2b727
orianig <oriana.ig016@gmail.com>
2023-06-28 19:21:24 +0200

    feat: add new routes

--- src/routers/app.router.jsx
+++ src/routers/app.router.jsx
@@ -1,23 +1,31 @@
-import { BrowserRouter, Route, Routes, Navigate  } from "react-router-dom"
-import Login from "../pages/Login/Login.page"
-import Register from "../pages/Register/Register.page"
-import Profile from "../pages/Dashboard/Profile.page"
-import NotFound from "../pages/404/notFound.page"
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Login from "../pages/Login/Login.page";
+import Register from "../pages/Register/Register.page";
+import Profile from "../pages/Dashboard/Profile.page";
+import NotFound from "../pages/404/notFound.page";
 
 export const AppRouter = () => {
   return (
     /* El BrowserRouter en clase lo colocamos en el main, pero prefiero que 
     la lógica de enrutado este en un solo lugar */
     <BrowserRouter>
-        <Routes>
-            <Route path="*" element={<NotFound />} />
-            <Route path="/login" element={ <Login /> } />
-            <Route path="/" element={ <Profile /> } />
-            <Route path="/register" element={ <Register /> } />
-        </Routes>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<LayoutAdmin />}>
+          <Route index element={<Profile />} />
+          {/* <Route path="appointments" element={<Appointments />} />
+          <Route path="patients" element={<Patients />} />
+          <Route path="doctors" element={<Doctors />} />
+          <Route path="appointmentManagement" element={<AppointmentManagement />} />
+          <Route path="treatments" element={<Treatments />} />
+          <Route path="interventions" element={<Interventions />} /> */}
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 // import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 // import { useSelector } from "react-redux";
