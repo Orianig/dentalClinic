2db4e06f93621dc3bc264ba10fccb56527c57504
orianig <oriana.ig016@gmail.com>
2023-06-28 23:35:06 +0200

    fix: change grammatical error

--- src/pages/Dashboard/Profile.page.jsx
+++ src/pages/Dashboard/Profile.page.jsx
@@ -1,4 +1,4 @@
-// // import React, { useState } from "react";
+import React, { useState } from "react";
 // // import { Menu, Transition } from "@headlessui/react";
 // import { CgProfile} from "react-icons/cg";//profile
 // // import { FaUserDoctor} from "react-icons/fa";//doctor
@@ -7,33 +7,9 @@
 
 const Profile = () => {
   return (
-//     <div className="min-h-screen">
-//       {/* Sidebar */}
-      <div className="fixed w-96 h-full bg-gray-100">Sidebar</div>
-//       {/* COMPONENTES DEL SIDEBAR */}
-//       {/* LOGO */}
-//       <div>
-//           <h1 className="text-2xl uppercase font-bold mb-10">LOGO</h1>
-//           <ul>
-//             <li>
-//               <a
-//                 href="#"
-//                 className="flex items-center gap-4 hover:bg-gray-200 transition-colors py-2 px-4 rounded-lg"
-//               >
-//                 <CgProfile /> Perfil
-//               </a>
-//             </li>
-//             <li>
-//               <a
-//                 href="#"
-//                 className="flex items-center gap-4 hover:bg-gray-200 transition-colors py-2 px-4 rounded-lg"
-//               >
-//                 <BsCalendarCheck /> Citas
-//               </a>
-//             </li>
-//             </ul>
-//         </div>
-//     </div>
+    <div>
+        <h1>profile</h1>
+    </div>
   );
 };
 
