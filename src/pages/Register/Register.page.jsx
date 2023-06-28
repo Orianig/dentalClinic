c73b21b16465efc5645f74217d7fe62587f2d473
orianig <oriana.ig016@gmail.com>
2023-06-28 21:08:12 +0200

    style: change the register page style

--- src/pages/Register/Register.page.jsx
+++ src/pages/Register/Register.page.jsx
@@ -1,7 +1,9 @@
-import React, { useState} from "react";
-import { toast } from 'react-toastify';
+import React, { useState } from "react";
+import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import { register } from "../../services/auth.service";
+import "../Login/Login.css";
+import Logo from "../../assets/images/logo/logo-7.png";
 //Hooks
 //se usa el hook para establecer el estado del usuario
 //estado inicial =>(lo que requiero)
@@ -34,153 +36,121 @@ const Register = () => {
     register(user)
       .then((result) => {
         // console.log("Registro exitoso:", result);
-        toast.success(user.name + ' te haz registrado con exito');
+        toast.success(user.name + " te haz registrado con exito");
         setTimeout(() => {
           navigate("/login");
         }, 2000);
       })
       .catch((error) => {
         // console.error("Error en el registro:", error);
-        toast.error('No se han cumplido los parametros necesarios');
+        toast.error("No se han cumplido los parametros necesarios");
       });
   };
 
   return (
     <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Crea una cuenta nueva
-          </h2>
+      <div className="register flex flex-col min-h-screen rounded-lg md:p-8">
+        <div className=" mb-6">
+          <img className="h-44 w-auto" src={Logo} alt="Company" />
         </div>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
+        <div className="p-8">
+          <h1 className="text-6xl text-white font-medium mb-2">
+            Crea una cuenta
+          </h1>
+          <span className="text-gray-500 font-medium">
+            ¿Ya eres usuario?{" "}
+            <a
+              href="#"
+              className="text-primary hover:underline"
+              onClick={() => navigate("/login")}
+            >
+              Ingresa
+            </a>
+          </span>
+          <form className="mt-8">
             {/* USERNAME */}
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="name"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Nombre
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  value={user.name}
-                  onChange={handleChange}
-                  id="name"
-                  name="name"
-                  type="text"
-                  autoComplete="name"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            {/* LASTNAME */}
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="lastName"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Apellido
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  value={user.lastName}
-                  onChange={handleChange}
-                  id="lastName"
-                  name="lastName"
-                  type="text"
-                  autoComplete="lastName"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            {/* EMAIL */}
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="Email"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Correo electronio
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  value={user.email}
-                  onChange={handleChange}
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+            <div className="max-w-lg mb-4 flex flex-col md:flex-row items-center justify-between gap-4 ">
+              <input
+                value={user.name}
+                onChange={handleChange}
+                id="name"
+                name="name"
+                type="text"
+                autoComplete="name"
+                required
+                className="w-full py-3 px-4 rounded-xl outline-none bg-[#343434] text-gray-100 group focus:ring-2 focus:ring-primary"
+                placeholder="Nombre"
+              />
+              {/* LASTNAME */}
+              <input
+                value={user.lastName}
+                onChange={handleChange}
+                id="lastName"
+                name="lastName"
+                type="text"
+                autoComplete="lastName"
+                required
+                className="w-full py-3 px-4 rounded-xl outline-none bg-[#343434] text-gray-100 group focus:ring-2 focus:ring-primary"
+                placeholder="Apellidos"
+              />
             </div>
             {/* PHONE NUMBER */}
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="phoneNumber"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Numero de telefono
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  value={user.phoneNumber}
-                  onChange={handleChange}
-                  id="phoneNumber"
-                  name="phoneNumber"
-                  type="text"
-                  autoComplete="phoneNumber"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+            <div className="max-w-lg mb-4">
+              <input
+                value={user.phoneNumber}
+                onChange={handleChange}
+                id="phoneNumber"
+                name="phoneNumber"
+                type="text"
+                autoComplete="phoneNumber"
+                required
+                className="w-full py-3 px-4 rounded-xl outline-none bg-[#343434] text-gray-100 group focus:ring-2 focus:ring-primary"
+                placeholder="Teléfono"
+              />
+            </div>
+            {/* EMAIL */}
+            <div className="max-w-lg mb-4">
+              <input
+                value={user.email}
+                onChange={handleChange}
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="w-full py-3 px-4 rounded-xl outline-none bg-[#343434] text-gray-100 group focus:ring-2 focus:ring-primary"
+                placeholder="Correo electrónico"
+              />
             </div>
             {/* PASSWORD */}
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Password
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  value={user.password}
-                  onChange={handleChange}
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+            <div className="max-w-lg mb-4">
+              <input
+                value={user.password}
+                onChange={handleChange}
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="w-full py-3 px-4 rounded-xl outline-none bg-[#343434] text-gray-100 group focus:ring-2 focus:ring-primary"
+                placeholder="Contraseña"
+              />
             </div>
-            <div>
+            <div className="max-w-lg flex justify-center md:justify-end mb-6">
+              <a
+                href="#"
+                className="text-gray-500 font-medium hover:text-gray-300 transition-colors"
+              >
+                ¿Olvidaste tu contraseña?
+              </a>
+            </div>
+            <div className="max-w-lg flex justify-center md:justify-end mb-6">
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                onClick={(e) => { handleSubmit(e)}}
+                className="bg-primary text-white w-48 py-3 px-4 rounded-full hover:bg-cyan-700 transition-colors"
+                onClick={(e) => {
+                  handleSubmit(e);
+                }}
               >
                 Crear cuenta
               </button>
