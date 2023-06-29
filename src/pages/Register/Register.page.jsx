b66f000404ee9a5320a3ef4ff7e84f843d9226c3
orianig <oriana.ig016@gmail.com>
2023-06-29 15:57:26 +0200

    refactor: delete unnecesary items

--- src/pages/Register/Register.page.jsx
+++ src/pages/Register/Register.page.jsx
@@ -49,11 +49,11 @@ const Register = () => {
 
   return (
     <>
-      <div className="register flex flex-col min-h-screen rounded-lg md:p-8">
-        <div className=" mb-6">
+      <div className="register flex flex-col min-h-screen rounded-lg md:p-2">
+        <div className=" mb-0">
           <img className="h-44 w-auto" src={Logo} alt="Company" />
         </div>
-        <div className="p-8">
+        <div className="p-2 ml-8">
           <h1 className="text-6xl text-white font-medium mb-2">
             Crea una cuenta
           </h1>
