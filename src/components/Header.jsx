69c3cb620372906b07ccb4beb8c7f86235b36335
orianig <oriana.ig016@gmail.com>
2023-06-29 23:32:55 +0200

    feat: add show user first and last name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
+
 
 const Header = () => {
+  const name = useSelector(state => state.user.data.name);
+  const lastName = useSelector(state => state.user.data.lastName);
+  console.log(name)
+  console.log(lastName)
   return (
-    <header className="h-[7vh] md:h-[10vh] border-b border-secondary-100 p-8">
-        header
-    </header>
-  );
+    <>
+     <header className="h-[7vh] md:h-[10vh] border-b border-secondary-100 p-8 flex items-center justify-end">
+        <span className="text-md">{name + " " + lastName}</span>
+
+     </header>
+     </>
+  )
 };
 
 export default Header;
