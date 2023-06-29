6846c704f91fa32f9908f3611fe3ffe424f3de59
orianig <oriana.ig016@gmail.com>
2023-06-29 15:56:54 +0200

    feat: add menu button (show sidebar responsive)

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 //ICONS
 import { FaUser, FaUserMd, FaUsers } from "react-icons/fa"; //profile - doctor - pacientes - citas
 import { BsCalendarPlusFill } from "react-icons/bs"; //citas
-import { RiFolderUserFill, RiServiceFill } from "react-icons/ri"; //gestion de citas - servicios
+import {
+  RiFolderUserFill,
+  RiServiceFill,
+  RiLogoutCircleRLine,
+  RiCloseLine,
+  RiMenu3Line,
+} from "react-icons/ri"; //gestion de citas - servicios - logout
 
 const Sidebar = () => {
+  // se usa el hook para tener una variable de estado y una funcion para actualizarla
+  //showmenu se inicia en falso (no se muestra)
+  //al llamar a setshowmenu se pasa de visible a no visible  el menu
+  const [showMenu, setShowMenu] = useState(false);
+  // const [showSubmenu, setShowSubmenu] = useState(false);
   return (
     <>
-      <div className="xl:h-[100vh] overflow-y-scroll fixed xl:static w-full h-full -lrft-full top-0 bg-secondary-100 p-8">
+      <div
+        className={`xl:h-[100vh] overflow-y-scroll fixed xl:static w-[80%] md:w-[40%] lg:w-[30%] xl:w-auto h-full top-0 bg-secondary-100 p-4 flex flex-col justify-between z-50 ${
+          showMenu ? "left-0" : "-left-full"
+        } transition-all`}
+      >
         <div>
           <h1 className="text-center text-2xl font-bold text-gray-600 mb-10">
             ADMIN
@@ -58,7 +73,24 @@ const Sidebar = () => {
             </Link>
           </nav>
         </div>
+        <div>
+          <nav>
+            <Link
+              to="/"
+              className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors"
+            >
+              <RiLogoutCircleRLine className="text-primary" />
+              Cerrar sesión
+            </Link>
+          </nav>
+        </div>
       </div>
+      <button
+        onClick={() => setShowMenu(!showMenu)}
+        className="xl:hidden fixed bottom-4 right-4 bg-primary text-black p-3 rounded-full z-50"
+      >
+        {showMenu ? <RiCloseLine /> : <RiMenu3Line />}
+      </button>
     </>
   );
 };
