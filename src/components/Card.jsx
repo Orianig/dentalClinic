1d4e5294c152553a13c1255002ccacc4f33c25c7
orianig <oriana.ig016@gmail.com>
2023-07-01 02:43:37 +0200

    feat:add items and change the card structure and conditions

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,42 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ intervention, dentist, patient, date, showPatient, showDentist }) => {
+  const formattedDate = new Date(date).toLocaleString("es-ES", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
   return (
     <div className="bg-gray-100 p-2 md:p-2 mb-1 rounded-xl ">
       <div className="flex ml-24 justify-center ml-6 md:justify-between flex-wrap items-center gap-2 md:gap-4">
         {/* INTERVENTION */}
-        <div className="w-full md:w-1/4">
+        <div className="w-full md:w-1/5">
           <p className="text-sm md:text-base">
-            <span className="text-gray-600 font-semibold">44444</span>
+            <span className="text-gray-600 font-semibold">{intervention}</span>
           </p>
         </div>
         {/* DOCTOR */}
-        <div className="w-full md:w-1/4">
+        {showDentist && (
+        <div className="w-full md:w-1/5 ">
           <p className="text-sm md:text-base">
-            <span className="text-gray-600 font-semibold">44444</span>
+            <span className="text-gray-600 font-semibold">{dentist}</span>
           </p>
         </div>
+        )}
+        {/* PATIENT */}
+        {showPatient && (
+          <div className="w-full md:w-1/5 ">
+            <p className="text-sm md:text-base">
+              <span className="text-gray-600 font-semibold">{patient}</span>
+            </p>
+          </div>
+        )}
         {/* FECHA */}
-        <div className="w-full md:w-1/4">
+        <div className="w-full md:w-1/6">
           <p className="text-sm md:text-base">
-            <span className="text-gray-600 font-semibold">44444</span>
+            <span className="text-gray-600 font-semibold">{formattedDate}</span>
           </p>
         </div>
       </div>
